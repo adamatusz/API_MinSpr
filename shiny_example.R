17701f601440c10c30019abba8519a95dd1c6b97
adamatusz <adamatusz@gmail.com>
2025-07-26 16:58:15 +0200

    Usunięcie niepotrzebnych plików z projektu

--- shiny_example.R
+++
@@ -1,7 +0,0 @@
-install.packages("shiny")
-
-library(shiny)
-# Launches an app, with the app's source code included
-runExample("06_tabsets")
-# Lists more prepackaged examples
-runExample("09_upload")
